11ff22c3b2dcd791743538b1db6ce06556d3c671
lpolobeltran <luispolofb@gmail.com>
2024-07-22 12:50:24 -0500

    Se llega existosamente hasta el video 120

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
+
+// Se definen cada una de las rutas:
+const routes: Routes = [ // Esta interfaz viene incluida en Angular
+
+  // RUTA A HOME-PAGE
+  {
+    path: '', // Queremos que cuando el URL inicial sea el de home
+    component: HomePageComponent // Componente, en este caso pagina, a mostrar
+  },
+
+  // RUTA A ABOUT-PAGE
+  {
+    path: 'about', // Segmento que se le agrega a la URL, especifica la pagina a mostrar
+    component: AboutPageComponent // Componente, en este caso pagina, a mostrar
+  },
+
+  // RUTA A CONTACT-PAGE
+  {
+    path: 'contact', // Segmento que se le agrega a la URL, especifica la pagina a mostrar
+    component: ContactPageComponent // Componente, en este caso pagina, a mostrar
+  },
+
+  // RUTA AL MODULO COUNTRIES
+  {
+    path: 'countries', //
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule) // Al usar este path se carga el modulo country mediante una promesa
+  },
+
+  // RUTA POR DEFAULT (SINO HAY NINGUN PATH O CUALQUIER OTRO CASO)
+  {
+    path: '**', // Segmento que se le agrega a la URL, especifica la pagina a mostrar
+    redirectTo: ''
+    // component: HomePageComponent // Componente, en este caso pagina, a mostrar
+  }
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes) // Solo se configura asi si es el routing principal de la app
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+// Se definen cada una de las rutas:
+const routes: Routes = [ // Esta interfaz viene incluida en Angular
+
+  {
+    path: 'by-capital',
+    component: ByCapitalPageComponent,
+  },
+  {
+    path: 'by-country',
+    component: ByCountryPageComponent,
+  },
+  {
+    path: 'by-region',
+    component: ByRegionPageComponent,
+  },
+  {
+    path: 'by/:id', // se pondra un ID de manera dinamica
+    component: CountryPageComponent,
+  },
+
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class CountriesRoutingModule { }
